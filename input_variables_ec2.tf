2c1c75fc777476e77c72711b7089a9c94411b50e
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2022-10-22 20:42:05 +0530

    Create input_variables_ec2.tf

---
+++ input_variables_ec2.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-09d3b3274b6c5d4aa"
+  instance_type = "t2.micro"
+}
+variable "instance_name" {
+  description = "Value of the Name tag the EC2 instance"
+  type = string
+  default = "VM-1"
+}
+
+# terraform apply -var "instance_name=Linux-VM"
