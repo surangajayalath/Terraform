0341fd1c5cce80426344dd173748a94f65bafb3d
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2022-10-22 20:46:02 +0530

    Update input_variables_ec2.tf

--- input_variables_ec2.tf
+++ input_variables_ec2.tf
@@ -16,11 +16,16 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-09d3b3274b6c5d4aa"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
 }
 variable "instance_name" {
   description = "Value of the Name tag the EC2 instance"
   type = string
   default = "VM-1"
 }
+
 
 # terraform apply -var "instance_name=Linux-VM"
