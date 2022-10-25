c7e3cdb44f9719dbdc817f20b346417b7bfab44b
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2022-10-25 10:03:30 +0530

    Update example-1.tf

--- Meta_Argument/example-1.tf
+++ Meta_Argument/example-1.tf
@@ -1,4 +1,3 @@
-# This create % instance with same ami & with numbering tag using index value
 resource "aws_instance" "web" {
   ami = "ami-09d3b3274b6c5d4aa"
   instance_type = "t2.micro"
@@ -9,6 +8,15 @@ resource "aws_instance" "web" {
   tags = {
     "Name" = "EC2-VM ${count.index}"
   }
+  # Depends on S3
+  # If you want to create resource before another one use dependon
+  depends_on = [
+    aws_s3_bucket.mybucket
+  ]
 
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "sura_bucket"
 }
 
